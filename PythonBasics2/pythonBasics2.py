804e724a5bfc8cbde13b89a6582f43617ba684e1
Drew Robieson <arobieso@uncc.edu>
2021-10-06 19:04:56 -0400

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -10,35 +10,93 @@
 # returns the number of multiples of 3 in the range from 0
 # to n (including n).
 
+# Return multiple of 3 that occurs most in a string
 def count_threes(n):
-  # YOUR CODE HERE
-    count = 0
-    if n < 3:
-        return 0
-    for i in range(0, n):
-        if i % 3 == 0:
-            count = count + 1
-    return count
+    # YOUR CODE HERE
+    d = dict = {
+        "3": 0,
+        "6": 0,
+        "9": 0
+    }
+    for i in n:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] = d[i] + 1
 
+    if d["6"] > d["3"] and d["6"] > d["9"]:
+        return 6
+    elif d["9"] > d["3"] and d["9"] > d["6"]:
+        return 9
+    else:
+        return 3
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
+
+# Return longest repeating characters
 def longest_consecutive_repeating_char(s):
-  # YOUR CODE HERE
-    character = ''
-    count = 0
+    # YOUR CODE HERE
+    dictCurr = {}
+    dictHighest = {
+        'a': 0,
+        'b': 0,
+        'c': 0,
+        'd': 0,
+        'e': 0,
+        'f': 0,
+        'g': 0,
+        'h': 0,
+        'i': 0,
+        'j': 0,
+        'k': 0,
+        'l': 0,
+        'm': 0,
+        'n': 0,
+        'o': 0,
+        'p': 0,
+        'q': 0,
+        'r': 0,
+        's': 0,
+        't': 0,
+        'u': 0,
+        'v': 0,
+        'w': 0,
+        'x': 0,
+        'y': 0,
+        'z': 0,
+    }
     for i in range(0, len(s)):
-        current = 1
-        for j in range(i + 1, len(s)):
-            if s[i] == s[j]:
-                current = current + 1
+        # if values are equal
+        if i == len(s) - 1:
+            # handle reaching end of string
+            continue
+        if s[i] == s[i + 1]:
+            # if not in dictionary, then add it
+            if s[i] not in dictCurr:
+                dictCurr[s[i]] = 1
+            # else increment value
             else:
-                break
-        if current > count:
-            count = current
-            character = s[i]
-    return character
+                dictCurr[s[i]] += 1
+            # if curr value has surpassed highest chain value, assign new value to highest
+            if dictHighest.get(s[i]) < dictCurr.get(s[i]):
+                dictHighest[s[i]] = dictCurr.get(s[i])
+        # Values unequal, clear current
+        else:
+            dictCurr.clear()
+    # loop through dictHighest and find the largest values
+    largest = 0
+    arr = []
+    # Find the largest value
+    for value in list(dictHighest.values()):
+        if value > largest:
+            largest = value
+    # Add all keys with largest value to arr
+    for key in list(dictHighest.keys()):
+        if dictHighest[key] == largest:
+            arr.append(key)
+    return arr
 
 
 # Part C. is_palindrome
@@ -51,8 +109,8 @@ def is_palindrome(s):
     # YOUR CODE HERE
     s = s.lower()
     s = s.replace(" ", "")
-    #print(s)
+    # print(s)
     for i in range(0, len(s) // 2):
-        if s[i] != s[len(s)-i-1]:
+        if s[i] != s[len(s) - i - 1]:
             return False
     return True
