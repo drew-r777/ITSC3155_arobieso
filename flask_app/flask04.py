e2ed788d64c928cdca6c42e72a3f2e843c68a22c
Drew Robieson <arobieso@uncc.edu>
2021-11-02 09:14:08 -0400

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -8,26 +8,25 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
-
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 
-
 # Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
-    
+    db.create_all()  # run under the app context
 
-notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-         2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-         3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
-         }
+
+# notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+# 2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+# 3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+# }
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -36,19 +35,21 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 
 @app.route('/index')
 def index():
-    a_user = {'name': 'Drew', 'email': 'arobieso@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
     return render_template('index.html', user=a_user)
 
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name': 'Drew', 'email': 'arobieso@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    my_notes = db.session.query(Note).all()
     return render_template('notes.html', notes=notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Drew', 'email': 'arobieso@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
     return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
 
@@ -68,13 +69,14 @@ def new_note():
         today = date.today()
         # format date mm/dd/yyyy
         today = today.strftime("%m-%d-%Y")
-        # get last id used and increment by 1
-        id = len(notes) + 1
-        # create new note array
-        notes[id] = {'title': title, 'text': text, 'date': today}
+
+        new_record = Note(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
 
         return redirect(url_for('get_notes', name=user))
     else:
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
         return render_template('new.html', user=a_user)
 
 
